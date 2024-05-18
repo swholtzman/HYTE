2ca5bcd5fa7f101d6b51c02de81b34cbc7f61a58
Sam Holtzman <sam.wolf.holtzman@gmail.com>
2024-05-18 15:51:11 -0700

    added node server changes to each page links changes to landing page

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// index.js
+// HYTE/index.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'Main' directory
-app.use(express.static(path.join(__dirname, 'Main')));
+// Serve static files from the 'HYTE' directory
+app.use(express.static(path.join(__dirname)));
 
 // Send 'main.html' for the root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'Main', 'main.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
